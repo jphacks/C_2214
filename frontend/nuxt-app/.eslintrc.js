d90518fd498aee2d7721c6fe2cc25edba39ea4c7
unagibuchi <striker0219@gmail.com>
2022-10-16 11:39:05 +0900

    create nuxt app

---
+++ frontend/nuxt-app/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    'jest/globals': true,
+    node: true
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [
+    'jest'
+  ],
+  // add your custom rules here
+  rules: {}
+}
