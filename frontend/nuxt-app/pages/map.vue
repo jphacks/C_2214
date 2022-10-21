bb0d81f56f61094d1c5e9648757bb7dc022d26cd
Onda Reki <rekionda0524@ezweb.ne.jp>
2022-10-21 22:35:39 +0900

    mqplatform導入

--- frontend/nuxt-app/pages/map.vue
+++ frontend/nuxt-app/pages/map.vue
@@ -8,15 +8,29 @@ export default {
   head () {
     return {
       link: [
-        { href: 'https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.css', rel: 'stylesheet' }
+        { href: 'https://api.mapbox.com/mapbox-gl-js/v1.13.2/mapbox-gl.css', rel: 'stylesheet' }
       ],
       script: [
-        { src: 'https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.js', defer: true, body: true },
-        { src: 'https://prodmqpstorage.z11.web.core.windows.net/mqplatform.js', defer: true, body: true }
+        { src: 'https://api.mapbox.com/mapbox-gl-js/v1.13.2/mapbox-gl.js', defer: true }
+        // { src: 'https://prodmqpstorage.z11.web.core.windows.net/mqplatform.js', defer: true, body: true }
       ]
     }
   },
   mounted () {
+    function mqplatformTransformRequest (subscriptionKey) {
+      return (url, resourceType) => {
+        if (url.startsWith('mqplatform://')) {
+          return {
+            url: `${url.replace('mqplatform://', 'https://prod-mqplatform-api.azure-api.net/')}?subscription_key=${subscriptionKey}`
+          }
+        } else if (url.startsWith('dev-mqplatform://')) {
+          return {
+            url: `${url.replace('dev-mqplatform://', 'https://dev-mqp-api-management-developer.azure-api.net/')}?subscription_key=${subscriptionKey}`
+          }
+        }
+        return { url }
+      }
+    }
     const transformRequest = mqplatformTransformRequest('90b1699e2a884689a30987aefdd167f0')
     const map = new mapboxgl.Map({
       container: 'map',
